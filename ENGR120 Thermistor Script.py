01ca7675fe9daf183a3fd91542a260e1d60abc99
Arjunebug21 <arjunpathak0411@gmail.com>
2024-03-24 13:30:58 -0700

    Update ENGR120 Thermistor Script.py

--- ENGR120 Thermistor Script.py
+++ ENGR120 Thermistor Script.py
@@ -2,22 +2,53 @@ import machine
 import utime
 import math
 
-adcpin = 15
+adcpin = 15 
 thermistor = machine.ADC(adcpin)
+adcpin2 = 26
+thermistor2 = machine.ADC(adcpin2)
+
+#Initialize LED pin as output
+led = machine.Pin(25, machine.Pin.OUT)
+
+# Initialize photoresistor pins
+adcpin = 15
+photoresistor = machine.ADC(adcpin) 
+
+# Threshold values for the led. 
+threshold_sunny = 10000
+threshold_cloudy = 35000
+threshold_night = 5000
+
+# While loop that controls when the led turns on and off. If the the photoresistor reads a value greater than the sunny value, led will turn off.
+while True:
+    sensor_value = sensor_pin.read()
+    sleep(5)
+
+    if sensor_value >= threshold_sunny or sensor_value <= threshold_night :
+        led.off()
+
+    else:
+        led.on()
+
+
+    sleep(1)
 
 while True:
-    adc = thermistor.read_u16()
-    Vout = (3.3/65535)*adc #This line converts the adc value from a range from 0-65535 to a range 0 - 3.3
+    adc = thermistor.read_u16() #Temperature
+    Vout = (3.3/65535)*adc #This line converts the thermistor voltage value from a range from 0-65535 to a range 0 - 3.3
+    adc2 = thermistor2.read_u16() #Moisture
+    Vout2 = (3.3/65535)*adc2 #This line converts the thermistor2 value from a range from 0-65535 to a range 0 - 3.3
+
+    Moisture = -5*(thermistor2.thermistorTemp(Vout2)) + 105 #This value gives an approximate conversion of soil temperature to soil moisture 
 
     TempC = thermistor.thermistorTemp(Vout)
 
     print(round(TempC, 1))
+   
+    print(round(Moisture, 1), "%")
 
     sleep(5)
 
-# Voltage Divider
-Vin = 3.3
-Ro = 10000  # 10k Resistor
 
 def thermistorTemp(Vout):
 
